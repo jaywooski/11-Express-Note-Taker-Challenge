cd7d2e1ffa528635a49e963d2b0c420261a410be
Woogster <john.m.wooley@gmail.com>
2022-03-08 16:38:08 -0600

    Added id functionality and part of delete button functionality

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const router = require('express').Router();
 const db = require('../../db/db.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 
 // get api/notes ... should read db.json file and return all saved
@@ -20,7 +21,7 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const data = db;
     const note = req.body;
-    note.id = data.length + 1;
+    note.id = uuidv4();
 
     if (note) {
         data.push(note);
@@ -42,25 +43,18 @@ router.delete('/notes/:id', (req, res) => {
     const noteId = req.params.id;
     const data = db;
     
-    if(noteId <= data.length) {
+    if(noteId) {
         
         const result = data.filter(note => note.id != noteId);
-        const decreaseId = function(array, id) {
-            id == noteId; 
-            // if id == noteId, find index. Every object from that index on out, update id by -1;
-            index =  array.findIndex();
-            for(i=index; i <= array.length; i++){
-                array[i].id -= 1;
-            }
-            return array;
-        }
-        // const final = result.map(note => note.id -= 1)
-        // res.json(final)
-        res.json(decreaseId(result));
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify( data , null, 2)
+        );
+        res.json(result);
         
     }
     else {
-        res.send(404);
+        res.sendStatus(404);
     }   
 })
 
