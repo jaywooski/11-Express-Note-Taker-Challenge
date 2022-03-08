dd10aac21547cc91c134af8e846a869241bfed7a
Woogster <john.m.wooley@gmail.com>
2022-03-08 03:34:08 -0600

    Fixed routes in both API and HTML, working on updating ID after deleting object in db

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const router = require('express').Router();
-// const { getNotes, saveNote, deleteNote } = require('../../public/assets/js/index')
 const db = require('../../db/db.json');
 const fs = require('fs');
 
@@ -8,28 +7,29 @@ const fs = require('fs');
 // get api/notes ... should read db.json file and return all saved
 // notes as JSON
 router.get('/notes', (req, res) => {
-    let data = db;
-    let query = req.query;
+    const data = db;
+    const query = req.query;
     if(query.title) {
         data = data.filter(note => note.title === query.title)
     }
-    return data;
+    res.json(data);
 })
 
 // POST api/notes should recieve new note as req.body, add to db.json
 // file and return the new note to the client
 router.post('/notes', (req, res) => {
     const data = db;
-    let text = req.body;
-    text.id = data.length;
+    const note = req.body;
+    note.id = data.length + 1;
 
-    if (text) {
-        data.push(text);
+    if (note) {
+        data.push(note);
         fs.writeFileSync(
             path.join(__dirname, '../../db/db.json'),
-            JSON.stringify( { text : data }, null, 2)
+            JSON.stringify( data , null, 2)
         );
-        return text;
+        res.json(data);
+        
     } 
     else {
         res.status(400).send('Bad request, try entering again!');
@@ -38,16 +38,43 @@ router.post('/notes', (req, res) => {
 
 // Delete /api/notes/:id ... should recieve query param containing ID of a note
 // In order to delete
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const noteId = req.params.id;
-    let data = db;
-    const result = data.filter(note => note.id = noteId)[0]; 
-
-    if(result) {
-        res.json(result);
-    } else {
-        res.send(404);
+    const data = db;
+    
+    if(noteId <= data.length) {
+        
+        const result = data.filter(note => note.id != noteId);
+        const decreaseId = function(array) {
+            const setIdToNoteId = (id) => id == noteId; 
+            index =  array.findIndex(setIdToNoteId(array.id));
+            for(i=index; i <= array.length; i++){
+                array[i].id -= 1;
+            }
+            return array;
+        }
+        // const final = result.map(note => note.id -= 1)
+        // res.json(final)
+        res.json(decreaseId(result));
+        
     }
+    else {
+        res.send(404);
+    }   
+})
+
+router.get('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const data = db;
+    
+    if(noteId <= data.length) {
+
+        const result = data.filter(note => note.id == noteId); 
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }   
 })
 
 module.exports = router;
